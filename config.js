b76ec1d7e884ba01966d2f25ee9c60a6aa362a7f
plop14 <70171493+plero75@users.noreply.github.com>
2025-07-06 02:28:20 +0200

    Create  config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+export const API_PROXY="https://ratp-proxy.hippodrome-proxy42.workers.dev/?url=";
+export const STOPS={joinville:"stop_area:IDFM:87759009",hippodrome77:"stop_area:IDFM:44034",ecoleBreuil:"stop_area:IDFM:44021"};
+export const LINES={rerA:"line:RER:A",bus77:"line:BUS:77",bus201:"line:BUS:201"};
+export const ICONS={"RER A":"🅰️","Bus 77":"🚌","Bus 201":"🚌"};
+
